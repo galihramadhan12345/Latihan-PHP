1365c5098360d17b27b4a54104f56b6d3bb8d9b1
galihramadhan12345 <galihramadhan5678@gmail.com>
2022-12-15 16:17:06 +0700

    Latihan php

---
+++ do_while.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Penggunaan Kontrol Do While</h2>
+    Kelipatan 3 adalah
+    <?php
+    $i=0;
+    do{
+        $yar+-3;
+        echo ("$yar");
+        $i++;
+    }while ($i<=4)
+    ?>
+</body>
+</html>
